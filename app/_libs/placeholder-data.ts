802cfd1ecc75938108d349381be2a74d8b55a25d
yuta-2001 <yu0210ta.sug@gmail.com>
2023-11-18 15:19:39 +0700

    基本フォルダ構成作成

---
+++ app/_libs/placeholder-data.ts
@@ -0,0 +1,22 @@
+import { TTransaction } from './definitions';
+
+const transactions: TTransaction[] = [
+  {
+    'id': 1,
+    'opponent_id': 1,
+    'is_settled': false,
+    'type': 1,
+    'name': 'Lunch',
+    'amount': 10,
+    'memo': null,
+  },
+  {
+    'id': 2,
+    'opponent_id': 2,
+    'is_settled': false,
+    'type': 1,
+    'name': 'Dinner',
+    'amount': 20,
+    'memo': null,
+  }
+];

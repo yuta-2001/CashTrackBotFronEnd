da9316b5e57cfd92271751ef9385f8b6df2a1af2
yuta-2001 <yu0210ta.sug@gmail.com>
2023-11-22 12:21:44 +0700

    画像最適化

--- app/_components/common/HeaderComponent.tsx
+++ app/_components/common/HeaderComponent.tsx
@@ -1,4 +1,5 @@
 import { TUser } from '../../_libs/types'
+import Image from 'next/image'
 
 type HeaderComponentProps = {
   user: TUser | null
@@ -10,7 +11,18 @@ const HeaderComponent = (props: HeaderComponentProps) => {
   return (
     <header className="shadow p-4 py-3 flex items-center bg-green-500 w-full">
       <div className="w-12 h-12 bg-gray-300 rounded-full flex items-center justify-center overflow-hidden">
-        <img src={user?.pictureUrl} />
+        { 
+          user?.pictureUrl ? (
+            <Image
+              src={user.pictureUrl}
+              alt="user icon"
+            />
+          ) : (
+            <span className="block text-center">
+              仮
+            </span>
+          )
+        }
       </div>
       <h1 className="ml-4 text-lg text-white font-medium">{ user?.name }</h1>
     </header>

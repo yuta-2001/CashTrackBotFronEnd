94d0fe1a529230004d86fc6f2708dee4249f30ff
yuta-2001 <yu0210ta.sug@gmail.com>
2023-11-25 17:36:44 +0700

    相手の編集機能作成

--- app/opponents/page.tsx
+++ app/opponents/page.tsx
@@ -1,13 +1,33 @@
 'use client';
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { useRouter } from 'next/navigation';
-import { useOpponents } from '../_context/OpponentsProvider';
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useOpponents, useOpponentsUpdate } from '../_context/OpponentsProvider';
 import { useSearchConditionsUpdate } from '../_context/SearchConditionsProvider';
+import { TOpponent } from '../_libs/types';
+import ValidationErrorText from '../_components/common/ValidationErrorText';
+import { updateOpponent } from '../_libs/data';
+import { useLiff } from '../_context/LiffProvider';
+import { deleteOpponent } from '../_libs/data';
+
+type FormData = {
+  name: string;
+};
 
 export default function OpponentsPage() {
   const router = useRouter();
   const opponents = useOpponents();
+  const setOpponents = useOpponentsUpdate();
   const setSearchConditions = useSearchConditionsUpdate();
+  const liff = useLiff();
+
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState<boolean>(false);
+  const [targetEditOpponent, setTargetEditOpponent] = useState<TOpponent | null>(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+	} = useForm<FormData>();
 
   if (
     opponents === undefined ||
@@ -26,34 +46,125 @@ export default function OpponentsPage() {
     router.push('/transactions');
   }
 
-  return (
-    <div
-      className="flex-1 overflow-y-auto p-4 w-full fixed top-20"
-      style={{
-        height: 'calc(100vh - 10rem)',
-      }}
-    >
-      {opponents.map((opponent) => {
-        return (
-          <div className="mb-4 p-4 bg-white rounded-lg shadow flex items-center justify-between">
-            <div className="flex-grow ml-4">
-              <h3 className="text-lg font-semibold text-gray-800">中山ゆい</h3>
-            </div>
+  const onSubmit = useCallback<SubmitHandler<FormData>>(async (data) => {
+    if (targetEditOpponent === null || liff === null　|| opponents === undefined || setOpponents === undefined) {
+      return;
+    }
 
-            <div className="flex items-center">
+    const editedOpponent = await updateOpponent(targetEditOpponent.id, data, liff);
+    if (editedOpponent) {
+      setTargetEditOpponent(null);
+      setOpponents(opponents.map((opponent) => {
+        if (opponent.id === targetEditOpponent.id) {
+          return editedOpponent;
+        }
+        return opponent;
+      }));
+    }
+  }
+  , [targetEditOpponent, liff, opponents, setOpponents]);
+
+
+  const submitDeleteOpponent = useCallback(async (opponentId: number) => {
+    if (liff === null || opponents === undefined || setOpponents === undefined) {
+      return;
+    }
+
+    if (!confirm('選択した相手を削除しますか？削除した場合、相手に紐づいている取引も自動的に削除されます。')) {
+      return;
+    }
+
+    try {
+      await deleteOpponent(opponentId, liff);
+      setTargetEditOpponent(null);
+      setOpponents(opponents.filter((opponent) => {
+        return opponent.id !== opponentId;
+      }));
+    } catch (e) {
+      alert('エラーが発生しました');
+    }
+    
+  }, [liff, opponents, setOpponents]);
+
+
+  return (
+    <>
+      <div
+        className="flex-1 overflow-y-auto p-4 w-full fixed top-20"
+        style={{
+          height: 'calc(100vh - 10rem)',
+        }}
+      >
+        {opponents.map((opponent) => {
+          return (
+            <div key={opponent.id} className="mb-4 p-4 bg-white rounded-lg shadow flex items-center justify-between">
+              <div className="flex-grow ml-4">
+                <h3 className="text-lg font-semibold text-gray-800">{opponent.name}</h3>
+              </div>
+
+              <div className="flex items-center">
+                <button
+                  onClick={() => checkTransaction(opponent.id)}
+                  className="py-6 px-4 mr-2 text-md font-bold text-gray-500 bg-gray-200 rounded hover:bg-gray-300"
+                >
+                  貸借り確認
+                </button>
+                <button
+                  onClick={() => setTargetEditOpponent(opponent)}
+                  className="py-6 px-4 text-md font-bold text-gray-500 bg-gray-200 rounded hover:bg-gray-300"
+                >
+                  編集
+                </button>
+              </div>
+            </div>
+          )
+        })}
+
+        {targetEditOpponent && (
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex justify-center items-center z-50">
+            <div style={{ maxHeight: '90%' }} className="bg-white w-11/12 max-w-6xl h-auto overflow-auto rounded shadow-lg p-6 relative">
               <button
-                onClick={() => checkTransaction(opponent.id)}
-                className="py-6 px-4 mr-2 text-md font-bold text-gray-500 bg-gray-200 rounded hover:bg-gray-300"
+                onClick={() => setTargetEditOpponent(null)}
+                className="absolute top-3 right-3 text-gray-500 hover:text-gray-700"
               >
-                貸借り確認
+                ✖
               </button>
-              <button className="py-6 px-4 text-md font-bold text-gray-500 bg-gray-200 rounded hover:bg-gray-300">
-                編集
-              </button>
+              <h2 className="text-lg font-bold mb-4">編集</h2>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                {/* Name field */}
+                <div className="mb-4">
+                  <label htmlFor="name" className="block text-gray-700 text-sm font-bold mb-2">
+                    項目名
+                  </label>
+                  <input
+                    id="name"
+                    type="text"
+                    className="mt-1 block w-full h-10 p-2 border border-gray-300 bg-white rounded shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="項目名"
+                    defaultValue={targetEditOpponent.name}
+                    {...register("name", { required: '項目名は必須です' })} 
+                  />
+                  {errors.name && <ValidationErrorText message={errors.name.message} />}
+                </div>
+                <div className="flex justify-center mt-4">
+                  <button onClick={() => setTargetEditOpponent(null)} className="px-3 py-3 mr-2 bg-gray-300 rounded shadow hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 transition-colors">キャンセル</button>
+                  <button type="submit" className="px-3 py-3 bg-green-500 text-white rounded shadow hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-700 focus:ring-opacity-50 transition-colors">更新する</button>
+                  <button onClick={() => submitDeleteOpponent(targetEditOpponent.id)} className="px-3 py-3 ml-2 bg-red-500 text-white rounded shadow hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-700 focus:ring-opacity-50 transition-colors">削除する</button>
+                </div>
+              </form>
             </div>
           </div>
-        )
-      })}
-    </div>
+        )}
+
+      </div>
+
+
+      <button
+        className="fixed z-10 bottom-24 left-1/2 transform -translate-x-1/2 bg-green-500 hover:bg-green-600 text-white font-bold w-16 h-16 rounded-full leading-none text-3xl"
+        aria-label="Add"
+      >
+      +
+      </button>
+    </>
   );
 }

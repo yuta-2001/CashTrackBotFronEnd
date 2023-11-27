ec974bc63bf15e603054c44592c06260f0a467cd
yuta-2001 <yu0210ta.sug@gmail.com>
2023-11-27 22:25:50 +0700

    liffのURLごとにLIFFIDを振り分ける

--- app/_context/LiffProvider.tsx
+++ app/_context/LiffProvider.tsx
@@ -1,5 +1,6 @@
 'use client'
 import React, { createContext, useState, useEffect, ReactNode, useContext } from 'react';
+import { useRouter } from 'next/router'
 import liff, { Liff } from '@line/liff'
 
 
@@ -11,10 +12,20 @@ const LiffContext = createContext<Liff | null>(null)
 
 export const LiffProvider: React.FC<LiffProviderProps> = ({ children }) => {
   const [liffObject, setLiffObject] = useState<Liff | null>(null)
+  const router = useRouter();
 
   useEffect(() => {
+    let liffIdFromUrl = ''
+    if (router.pathname === '/transactions') {
+      liffIdFromUrl = process.env.NEXT_PUBLIC_LIFF_ID_TRANSACTION!
+    } else if (router.pathname === '/opponents') {
+      liffIdFromUrl = process.env.NEXT_PUBLIC_LIFF_ID_OPPONENT!
+    } else {
+      return
+    }
+
     liff
-      .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID! })
+      .init({ liffId: liffIdFromUrl })
       .then(() => {
         setLiffObject(liff)
 

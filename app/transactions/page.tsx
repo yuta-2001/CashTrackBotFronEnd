9a7d07c8474c2bf29ac98a04e528cad7392df03c
yuta-2001 <yu0210ta.sug@gmail.com>
2023-12-23 14:42:09 +0700

    ローディング追加

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -4,9 +4,19 @@ import HeadBtnListComponent from '../_components/Transactions/HeadBtnList/HeadBt
 import CreateTransactionComponent from '../_components/Transactions/CreateTransaction/CreateTransactionComponent';
 import ResultListComponent from '../_components/Transactions/ResultList/ResultListComponent';
 import { SelectedTransactionsProvider } from '../_context/Transactions/SelectedTransactionsProvider';
+import { useLiff } from '../_context/LiffProvider';
 
 
 export default function TransactionsPage() {
+  const liff = useLiff();
+  if (liff === null) {
+    return (
+      <div className="flex items-center justify-center h-screen w-screen bg-green-600 fixed z-50">
+        <div className="text-white animate-pulse text-4xl font-bold">loading...</div>
+      </div>
+    );
+  }
+
   return (
     <SelectedTransactionsProvider>
       <HeadBtnListComponent />

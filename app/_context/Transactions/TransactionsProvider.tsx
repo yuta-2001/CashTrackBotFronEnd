8990abc8901c8efbbd50cf31691d592d03287bf0
yuta-2001 <yu0210ta.sug@gmail.com>
2023-12-23 14:28:03 +0700

    エラーハンドリングリファクタ

--- app/_context/Transactions/TransactionsProvider.tsx
+++ app/_context/Transactions/TransactionsProvider.tsx
@@ -28,18 +28,7 @@ export const TransactionsProvider: React.FC<TransactionsProviderProps> = ({ chil
       setTransactions(transactionsData);
     }
 
-    try {
-      fetchData();
-    } catch (error) {
-      liff.sendMessages([{
-        type: 'text',
-        text: 'エラーが発生しました。時間をおいて再度お試しください。'
-      }]).then(() => {
-        liff.closeWindow();
-      }).catch((error) => {
-        console.log(error);
-      });
-    }
+    fetchData();
   }, [liff, getTransactions])
 
   return (
